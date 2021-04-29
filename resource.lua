8a9ca05fb777d2b7f4776da435cb932198827f0a
Al Mohaimin Farabi <73338180+al-mohaimin-farabi@users.noreply.github.com>
2021-04-29 23:15:11 +0600

    all file uploaded for 1

---
+++ resource.lua
@@ -0,0 +1,12 @@
+files {
+    'index.html',
+    'keks.css',
+    'bankgothic.ttf',
+    'intro.gif',
+    'intro.mp3',
+
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
